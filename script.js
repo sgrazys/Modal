3d6c919860829fe04d1ac1339271b15bdbc5a95b
sgrazys <s.grazys@gmail.com>
2023-03-28 15:11:59 +0300

    DOM class manipulation

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpneModal = document.querySelectorAll('.show-modal');
+
+// console.log(btnsOpneModal);
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsOpneModal.length; i++)
+  btnsOpneModal[i].addEventListener('click', showModal);
+
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
