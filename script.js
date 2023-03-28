df7821fcb1b601ab433c562053682742eb2927fa
sgrazys <s.grazys@gmail.com>
2023-03-28 16:08:55 +0300

    keypress event & handling esc

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ const closeModal = function () {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+  console.log(e.key);
+})
